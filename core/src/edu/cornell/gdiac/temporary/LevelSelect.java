3d89936734651b0accd029ab8ca7da63e1d4b31c
Melanie Gao <zg66@cornell.edu>
2023-04-14 14:06:59 -0400

    refactored code

---
+++ core/src/edu/cornell/gdiac/temporary/LevelSelect.java
@@ -0,0 +1,30 @@
+package edu.cornell.gdiac.temporary;
+
+public class LevelSelect {
+    private int numLevels;
+
+    private boolean hasPlayed;
+
+    private Level[] allLevels;
+
+    public LevelSelect(GameCanvas canvas) {
+    }
+
+
+    public void loadLevels(){
+
+
+
+    }
+
+
+    public void draw(GameCanvas canvas, int numLevels, Level[] allLevels){
+
+        for (Level l: allLevels){
+            int currNum = l.getLevelNumber();
+        }
+
+
+    }
+    
+}
