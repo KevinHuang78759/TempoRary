a0f099052934d6078d04e63de32bf1225e74940c
Melanie Gao <zg66@cornell.edu>
2023-04-28 13:12:40 -0400

    added draw methods

--- core/src/edu/cornell/gdiac/temporary/entity/BandMember.java
+++ core/src/edu/cornell/gdiac/temporary/entity/BandMember.java
@@ -331,6 +331,27 @@ public class BandMember {
         }
     }
 
+
+    /**
+     * Draw perfect hit indicator
+     */
+    public void drawPerfectHit(GameCanvas canvas){
+
+    }
+    /**
+     * Draw ok hit indicator
+     */
+    public void drawOKHit(GameCanvas canvas){
+
+    }
+
+    /**
+     * Draw good miss indicator
+     */
+    public void drawMissHit(GameCanvas canvas){
+
+    }
+
     private float borderthickness;
     /**
      * Draw the border
