6ab58aa829363198dd6d378a0afeb830395093cb
mluo24 <miranda.luo24@gmail.com>
2023-05-10 19:38:24 -0400

    added some retrieve and save methods

--- core/src/edu/cornell/gdiac/temporary/SaveManager.java
+++ core/src/edu/cornell/gdiac/temporary/SaveManager.java
@@ -5,18 +5,27 @@ import com.badlogic.gdx.Preferences;
 
 /** This class utilizes LibGDX Preferences to store saved games */
 public class SaveManager {
-    // TODO: ADD CLASS THAT USES CLASSES FOR THE DATA FROM ARCHSPEC?
     /** Singleton save manager */
     private static SaveManager saveManager;
-    /** Preferences for best times */
-    private final Preferences[] levels;
-    /** Preferences for user settings */
-    private final Preferences settings;
+    /** LibGDX Preferences for all the game's levels */
+    private Preferences[] levels;
+    /** LibGDX Preferences for user volume settings */
+    private Preferences volumeSettings;
+    /** LibGDX preferences for hit bindings */
+    private Preferences hitBindingSettings;
+    /** LibGDX preferences for switch bindings */
+    private Preferences[] switchBindingSettings;
+
 
     private SaveManager() {
         // TODO: UPDATE THIS WITH A LOOP OF ALL OF OUR LEVELS
-        this.levels = new Preferences[]{Gdx.app.getPreferences("edu.cornell.gdiac.temporary.levels")};
-        this.settings = Gdx.app.getPreferences("edu.cornell.gdiac.temporary.settings");
+        levels = new Preferences[]{Gdx.app.getPreferences("edu.cornell.gdiac.temporary.levels")};
+        volumeSettings = Gdx.app.getPreferences("edu.cornell.gdiac.temporary.settings.volume");
+        hitBindingSettings = Gdx.app.getPreferences("edu.cornell.gdiac.temporary.settings.hitBindingSettings");
+        switchBindingSettings = new Preferences[3];
+        for (int i = 2; i <= 4; i++) {
+            switchBindingSettings[i-2] = Gdx.app.getPreferences("edu.cornell.gdiac.temporary.settings.switchBindingSettings." + i);
+        }
     }
 
     /**
@@ -41,23 +50,32 @@ public class SaveManager {
     }
 
     // TODO: SAVE SETTINGS UPON EXIT
-    public void saveSettings() {
-
+    public void saveSettings(float musicVol, float fxVol) {
+        volumeSettings.putFloat("music", musicVol);
+        volumeSettings.putFloat("fx", fxVol);
     }
 
     // TODO: RETRIEVE THE KEYBINDINGS THAT USER SAVED
-    public int[] getKeybindingSettings() {
-        return null;
+    public int[] getHitKeybindingSettings() {
+        int[] temp = new int[4];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = hitBindingSettings.getInteger("i");
+        }
+        return temp;
     }
 
-    // TODO: RETRIEVE MUSIC VOLUME
+    /**
+     * Retrieves save data for music volume
+     */
     public float getMusicVolume() {
-        return 0;
+        return volumeSettings.getFloat("music");
     }
 
-    // TODO: RETRIEVE SOUND FX VOLUME
+    /**
+     * Retrieves save data for soundFX volume
+     */
     public float getFXVolume() {
-        return 0;
+        return volumeSettings.getFloat("fx");
     }
 
     // TODO: CLEAR SAVED DATA
